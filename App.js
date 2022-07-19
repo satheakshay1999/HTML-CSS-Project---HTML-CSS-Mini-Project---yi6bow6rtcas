c691c358490e23b635c0c623088dd9357d8c3a45
satheakshay1999 <88333068+satheakshay1999@users.noreply.github.com>
2022-07-19 17:15:51 +0530

    js file updated

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
-
+var navlinks = document.getElementById("navlinks");
+function showmenu(){
+    navlinks.style.right = "0";
+}
+function hidemenu(){
+    navlinks.style.right = "-200px"
+}
+AOS.init();
